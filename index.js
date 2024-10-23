6826ef71a20250b5568d1f6fa9bab0eb22e7de44
OleksandrKnyovets bc2024-3 <sashakn2018@gmail.com>
2024-10-23 23:04:13 +0300

    додав адекватне пояснення

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("node:fs"); //для роботи з файлами
 
 //ініціалізація командного інтерфейсу
 prog
-.option("-i, --input <path>",)  //Шлях до input файлу
+.option("-i, --input <path>",)                  //Шлях до input файлу
 .option("-o, --output <path>", "output.txt")    //Шлях до output файлу
 .option("-d, --display");                       //Виведення в консоль
 
@@ -15,24 +15,27 @@ prog
 prog.parse(process.argv);
 const {input, output, display} = prog.opts();
 
-//Перевірка наявності та читання файлу
-if (!input) 
-    throw new Error("Please, specify input file");
-if (!fs.existsSync(input)) 
-    throw new Error("Cannot find input file");
-
+//перевірка наявності та читання файлу
+if (!input){ //переввірка чи інпут (див. рядок 10) порожній 
+    console.log("Please, specify input file"); 
+    process.exit(); //завершення роботи програми 
+}
+if (!fs.existsSync(input)){  //перевірка існування файлу вказаний в інпуті
+    console.log("Cannot find input file");
+    process.exit();  
+}
 //читання вмісту input файлу
-const fileContent = fs.readFileSync(input, "utf-8");
+const fileContent = fs.readFileSync(input, "utf-8"); //дозволяє !синхронно! прочитати вміст файлу (див. рядок 10)
 
 //обробка JSON та фільтрація даних
-const data = JSON.parse(fileContent).filter((item) => item.value > 5 && item.ku > 13 );  //нема ku зі значенням 13 тому буде > 13
+const data = JSON.parse(fileContent).filter((item) => item.value > 5 && item.ku > 13 );  //нема ku зі значенням 13 тому буде > 13  (ааааа)
 
 //формування рядка з результатом
-const result = data.map((item) => item.value).join('\n');
+const result = data.map((item) => item.value).join('\n'); 
 
 //запис у файл якщо вказано параметр output
 if (output){
-    fs.writeFileSync(output, result);
+    fs.writeFileSync(output, result); //синхронно записує значення змінної result у вказаний файл. (див. рядок 11)
 }
 //виведення у консоль якщо активовано параметр display
 if (display){ 
